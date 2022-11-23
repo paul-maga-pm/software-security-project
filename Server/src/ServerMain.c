917289199f1db96450d494591f48ddc816ddf887
paul-maga-pm <93886764+paul-maga-pm@users.noreply.github.com>
2022-11-23 22:11:59 +0200

    Finished Server Impl

--- Server/src/ServerMain.c
+++ Server/src/ServerMain.c
@@ -8,6 +8,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "shlwapi.h"
+#include <ctype.h>
+
 // Need to link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
 // #pragma comment (lib, "Mswsock.lib")
@@ -17,8 +20,13 @@
 
 void handleClientRequests(SOCKET clientSocket);
 
-int __cdecl main(void)
-{
+const char* CURRENT_WORKING_DIR;
+
+int __cdecl main(int argc, char** argv) {
+
+    CURRENT_WORKING_DIR = argv[0];
+    printf(CURRENT_WORKING_DIR);
+
     WSADATA wsaData;
     int iResult;
 
@@ -94,11 +102,7 @@ int __cdecl main(void)
         handleClientRequests(ClientSocket);
      
         // shutdown the connection since we're done
-        iResult = shutdown(ClientSocket, SD_SEND);
-        if (iResult == SOCKET_ERROR) {
-            printf("shutdown failed with error: %d\n", WSAGetLastError());
-            closesocket(ClientSocket);
-        }
+        
 
         // cleanup
         printf("Client disconnected\n");
@@ -110,12 +114,72 @@ int __cdecl main(void)
     return 0;
 }
 
+int checkMetaChars(char* path) {
+    for (; *path; path++) {
+        if (!isalnum(*path) && *path != '_' && *path != '.' && *path != '/' && *path != ' ' && *path != '-') {
+            return 0;
+        }
+    }
+    return 1;
+}
 
+
+int getFullPath(char* partialPath, char* fullPathOutput) {
+    if (_fullpath(fullPathOutput, partialPath, _MAX_PATH) != NULL) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+
+int checkTraversal(char* path) {
+    if (!PathIsRelativeA(path)) {
+        return 0;
+    }
+
+    char fullPath[_MAX_PATH];
+    if (!getFullPath(path, fullPath)) {
+        return 0;
+    }
+
+    char currentWorkingDirFullPath[_MAX_PATH];
+    if (!getFullPath(".", currentWorkingDirFullPath)) {
+        return 0;
+    }
+
+    for (int i = 0; currentWorkingDirFullPath[i]; i++) {
+        if (fullPath[i] == NULL) {
+            return 0;
+        }
+
+        if (currentWorkingDirFullPath[i] != fullPath[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int validatePath(char* path) {
+    return checkMetaChars(path) && checkTraversal(path);
+}
+
+#define PATH_OK "PATH_OK"
+#define PATH_ERR "PATH_ERR"
+#define FILE_OPEN_OK "FILE_OPEN_OK"
+#define FILE_OPEN_ERR "FILE_OPEN_ERR"
+
+#define PATH_OK_SIZE strlen(PATH_OK)
+#define PATH_ERR_SIZE strlen(PATH_ERR)
+#define FILE_OPEN_OK_SIZE strlen(FILE_OPEN_OK)
+#define FILE_OPEN_ERR_SIZE strlen(FILE_OPEN_ERR)
+
+#define BUFFER_SIZE 1025
 
 void handleClientRequests(SOCKET clientSocket) {
-    
-    const int BUFFER_SIZE = 1025;
-    char filePath[1025] = { 0 };
+
+    char filePath[BUFFER_SIZE] = { 0 };
 
     int recvResult = recv(clientSocket, filePath, BUFFER_SIZE, 0);
 
@@ -123,34 +187,58 @@ void handleClientRequests(SOCKET clientSocket) {
         printf("Error while receiving path: %d\n", WSAGetLastError());
         return;
     }
-    
+
     printf("Path received = %s\n", filePath);
+
+    int SUCCESS = 1;
+    if (!validatePath(filePath)) {
+        printf("Invalid path received!\n");
+        SUCCESS = 0;
+    }
 
 
     FILE* filePtr = NULL;
 
-    if (fopen_s(&filePtr, filePath, "w") != 0) {
+    if (!SUCCESS || fopen_s(&filePtr, filePath, "w") != 0) {
         printf("Couldn't open file %s\n", filePath);
-        return;
+        SUCCESS = 0;
     }
 
-    char buffer[1025] = { 0 };
+
+
     while (1) {
-        
-        int recvResult = recv(clientSocket, buffer, BUFFER_SIZE, 0);
+        char buffer[BUFFER_SIZE] = { 0 };
+        int recvResult = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0);
         if (recvResult == SOCKET_ERROR) {
             printf("Error while receiving buffer\n");
             break;
         }
-
+        buffer[BUFFER_SIZE - 1] = 0;
         if (recvResult == 0) {
             printf("Finished receiving buffers\n");
             break;
         }
 
-        printf("Received buffer size: %zu\n, Buffer: %s\n", strlen(buffer), buffer);
-        fwrite(buffer, sizeof(char), strlen(buffer), filePtr);
+        printf("Received buffer size: %zu\nBuffer: %s\n", strlen(buffer), buffer);
+
+        if (SUCCESS) {
+            fwrite(buffer, sizeof(char), strlen(buffer), filePtr);
+        }
     }
 
-    fclose(filePtr);
+    int iResult = shutdown(clientSocket, SD_RECEIVE);
+    if (iResult == SOCKET_ERROR) {
+        printf("shutdown failed with error: %d\n", WSAGetLastError());
+        closesocket(clientSocket);
+    }
+
+    char requestResult[BUFFER_SIZE] = { 0 };
+    if (SUCCESS) {
+        fclose(filePtr);
+        strncpy_s(requestResult, BUFFER_SIZE - 1,"SUCCESS", strlen("SUCCESS"));
+    } else {
+        strncpy_s(requestResult, BUFFER_SIZE - 1, "FAIL", strlen("FAIL"));
+    }
+
+    send(clientSocket, requestResult, strlen(requestResult), 0);
 }
